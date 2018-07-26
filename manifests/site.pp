43b2bdef0d4435efaee5eb1da1fc64e8fb855819
Sriramajeyam Sugumaran <S.Sugumaran@asos.com>
2018-07-26 16:49:07 +0100

    All in one role created

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,6 +2,10 @@ notice('Welcome to Puppet Control Repo')
 notice("${lookup('welcome')}")
 notice($fqdn)
 
+node 'allinone.dev.vm' {
+  include ::roles::allinone
+}
+
 node default {
   warning('No matching roles found')
 }
---
+++ site-modules/roles/manifests/allinone.pp
@@ -0,0 +1,3 @@
+class roles::allinone {
+  info("All In One Role")
+}
