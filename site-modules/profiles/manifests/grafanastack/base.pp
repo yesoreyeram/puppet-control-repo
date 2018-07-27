4593f7554a7cc979fae398fa5bca92c68f9b92cc
Sriramajeyam Sugumaran <S.Sugumaran@asos.com>
2018-07-27 13:10:28 +0100

    supervisor added to grafana stack

--- site-modules/profiles/manifests/grafanastack/base.pp
+++ site-modules/profiles/manifests/grafanastack/base.pp
@@ -3,6 +3,7 @@ class profiles::grafanastack::base {
   notice('Applying Grafana stack base profile')
   include ::profiles::grafanastack::python
   include ::profiles::grafanastack::pip
+  include ::profiles::grafanastack::supervisor
   include ::profiles::grafanastack::nginx
   include ::profiles::grafanastack::nodejs
 }
---
+++ site-modules/profiles/manifests/grafanastack/supervisor.pp
@@ -0,0 +1,14 @@
+# Class profiles::grafanastack::supervisot
+class profiles::grafanastack::supervisor {
+  include ::profiles::grafanastack::pip
+  class { 'supervisord':
+    unix_socket          => false,
+    inet_server          => true,
+    inet_server_hostname => lookup('privateip'),
+    inet_server_port     => '9001',
+    inet_auth            => false,
+    inet_username        => undef,
+    inet_password        => undef,
+  }
+  Class['::profiles::grafanastack::pip'] -> Class['supervisord']
+}
