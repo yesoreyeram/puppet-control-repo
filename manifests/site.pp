bef2134997edd7079189dae4da9c86b84d233509
Sriramajeyam Sugumaran <S.Sugumaran@asos.com>
2018-08-01 13:04:12 +0100

    Modified node names

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,7 +2,7 @@ notice('Welcome to Puppet Control Repo')
 notice("${lookup('welcome')}")
 notice($fqdn)
 
-node 'allinone.dev.vm', 'allinonec7.dev.vm' {
+node 'allinoneu16.dev.vm', 'allinonec7.dev.vm' {
   include "::roles::grafanastack::allinone"
 }
 
