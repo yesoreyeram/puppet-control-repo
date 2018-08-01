0190b6e19b7913b5071fb8068c49004fc87f5116
Sriramajeyam Sugumaran <S.Sugumaran@asos.com>
2018-08-01 13:03:45 +0100

    Added console logging for puppet apply

--- puppet.sh
+++ puppet.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
-echo "==========================================================";
-echo "Installing required Puppet modules";
-echo "==========================================================";
-echo "Puppet modules list - Before install/update";
-/opt/puppetlabs/bin/puppet module list --tree --modulepath=/opt/puppet/modules/
-echo "Installing modules from /opt/puppet/Puppetfile";
-/opt/puppetlabs/puppet/bin/r10k puppetfile install --puppetfile /opt/puppet/Puppetfile --moduledir /opt/puppet/modules/;
-echo "Puppet modules list - After install/update";
-/opt/puppetlabs/bin/puppet module list --tree --modulepath=/opt/puppet/modules/
-echo "==========================================================";
-echo "Applying puppet";
-echo "==========================================================";
-/opt/puppetlabs/bin/puppet apply --modulepath=/opt/puppet/modules/:/opt/puppet/site-modules/ --hiera_config=/opt/puppet/hiera.yaml /opt/puppet/manifests/site.pp -v;
-echo "Puppet apply completed";
-echo "==========================================================";
+mkdir -p /opt/puppet/logs/
+cp /opt/puppet/logs/puppetrun.log /opt/puppet/logs/puppetrun.previous.log 2>/dev/null
+echo "" > /opt/puppet/logs/puppetrun.log
+
+echo "==========================================================" # >> /opt/puppet/logs/puppetrun.log
+echo "$(date) INFO : Installing required Puppet modules" # >> /opt/puppet/logs/puppetrun.log
+echo "==========================================================" # >> /opt/puppet/logs/puppetrun.log
+echo "$(date) INFO : Puppet modules list - Before install/update" # >> /opt/puppet/logs/puppetrun.log
+/opt/puppetlabs/bin/puppet module list --tree --modulepath=/opt/puppet/modules/ # >> /opt/puppet/logs/puppetrun.log
+echo "$(date) INFO : Installing modules from /opt/puppet/Puppetfile" # >> /opt/puppet/logs/puppetrun.log
+/opt/puppetlabs/puppet/bin/r10k puppetfile install --puppetfile /opt/puppet/Puppetfile --moduledir /opt/puppet/modules/ # >> /opt/puppet/logs/puppetrun.log
+echo "$(date) INFO : Puppet modules list - After install/update" # >> /opt/puppet/logs/puppetrun.log
+/opt/puppetlabs/bin/puppet module list --tree --modulepath=/opt/puppet/modules/ # >> /opt/puppet/logs/puppetrun.log
+
+echo "==========================================================" # >> /opt/puppet/logs/puppetrun.log
+echo "$(date) INFO : Applying puppet" # >> /opt/puppet/logs/puppetrun.log
+echo "==========================================================" # >> /opt/puppet/logs/puppetrun.log
+/opt/puppetlabs/bin/puppet apply --modulepath=/opt/puppet/modules/:/opt/puppet/site-modules/ --hiera_config=/opt/puppet/hiera.yaml /opt/puppet/manifests/site.pp -v # >> /opt/puppet/logs/puppetrun.log
+
+echo "==========================================================" # >> /opt/puppet/logs/puppetrun.log
+echo "$(date) INFO : Puppet apply completed" # >> /opt/puppet/logs/puppetrun.log
+echo "==========================================================" # >> /opt/puppet/logs/puppetrun.log
 exit 0;
