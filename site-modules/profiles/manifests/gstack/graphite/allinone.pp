45d22e404c5af07005d59585642577413f4d2aa3
Sriramajeyam Sugumaran <S.Sugumaran@asos.com>
2018-08-02 10:27:20 +0100

    /opt/graphite/conf/carbon.conf template added

--- site-modules/profiles/manifests/gstack/graphite/allinone.pp
+++ site-modules/profiles/manifests/gstack/graphite/allinone.pp
@@ -1,6 +1,7 @@
 # Class profiles::gstack::graphite::allinone
 class profiles::gstack::graphite::allinone (
   Hash $grweb_local_settings = lookup('profiles::gstack::graphite::allinone::graphite_web::local_settings'),
+  Hash $carbon_conf_settings = lookup('profiles::gstack::graphite::allinone::graphite::carbon_conf_settings'),
 ){
   include ::profiles::gstack::graphite::base
   file { '/opt/graphite/webapp/graphite/local_settings.py':
@@ -10,5 +11,9 @@ class profiles::gstack::graphite::allinone (
   file { '/opt/graphite/conf/wsgi.py':
     ensure  => file,
     content => template('profiles/gstack/opt/graphite/conf/graphite.wsgi.py.erb'),
+  }
+  file { '/opt/graphite/conf/carbon.conf':
+    ensure  => file,
+    content => template('profiles/gstack/opt/graphite/conf/carbon.conf.erb'),
   }
 }
