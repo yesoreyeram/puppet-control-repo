e6118080cb4c4e603791bba65f768a72239ae41e
Sriramajeyam Sugumaran <S.Sugumaran@asos.com>
2018-08-02 11:00:40 +0100

    /opt/graphite/conf/storage-schemas.conf created

--- site-modules/profiles/manifests/gstack/graphite/allinone.pp
+++ site-modules/profiles/manifests/gstack/graphite/allinone.pp
@@ -1,8 +1,9 @@
 # Class profiles::gstack::graphite::allinone
 class profiles::gstack::graphite::allinone (
-  Hash $grweb_local_settings = lookup('profiles::gstack::graphite::allinone::graphite_web::local_settings'),
-  Hash $carbon_conf_settings = lookup('profiles::gstack::graphite::allinone::graphite::carbon_conf_settings'),
-  Tuple $whitelist_conf_settings = lookup('profiles::gstack::graphite::allinone::graphite::whitelist_conf_settings'),
+  Hash    $grweb_local_settings      = lookup('profiles::gstack::graphite::allinone::graphite_web::local_settings'),
+  Hash    $carbon_conf_settings      = lookup('profiles::gstack::graphite::allinone::graphite::carbon_conf_settings'),
+  Hash    $storage_schemas           = lookup('profiles::gstack::graphite::allinone::graphite::storage_schemas'),
+  Tuple   $whitelist_conf_settings   = lookup('profiles::gstack::graphite::allinone::graphite::whitelist_conf_settings'),
 ){
   include ::profiles::gstack::graphite::base
   file { '/opt/graphite/webapp/graphite/local_settings.py':
@@ -16,6 +17,10 @@ class profiles::gstack::graphite::allinone (
   file { '/opt/graphite/conf/carbon.conf':
     ensure  => file,
     content => template('profiles/gstack/opt/graphite/conf/carbon.conf.erb'),
+  }
+  file { '/opt/graphite/conf/storage-schemas.conf':
+    ensure  => file,
+    content => template('profiles/gstack/opt/graphite/conf/storage-schemas.conf.erb'),
   }
   file { '/opt/graphite/conf/whitelist.conf':
     ensure  => file,
