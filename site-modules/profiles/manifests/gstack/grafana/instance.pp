7e3cd8a264bee854100636abe0f5cfa85cf9008e
Sriramajeyam Sugumaran <S.Sugumaran@asos.com>
2018-08-09 11:47:54 +0100

    Supervisord removed for grafana instance

--- site-modules/profiles/manifests/gstack/grafana/instance.pp
+++ site-modules/profiles/manifests/gstack/grafana/instance.pp
@@ -16,6 +16,7 @@ define profiles::gstack::grafana::instance (
       ensure  => directory,
       group   => $grafanauser,
       owner   => $grafanauser ,
+      recurse => true,
       require => Class['::profiles::gstack::grafana::base']
   }
   mysql::db { "grafana-${instance_name}" :
@@ -27,7 +28,7 @@ define profiles::gstack::grafana::instance (
     "Extract Grafana ${instance_name}":
       path     => '/usr/bin:/usr/sbin:/bin',
       provider => shell,
-      command  => "tar -xzvf /opt/puppet/packages/grafana-${grafanaversion}.tar.gz -C /opt/grafana/${instance_name} --strip-components=1",
+      command  => "su - ${grafanauser} -c 'tar -xzvf /opt/puppet/packages/grafana-${grafanaversion}.tar.gz -C /opt/grafana/${instance_name} --strip-components=1'",
       unless   => "cat /opt/grafana/${instance_name}/VERSION | grep ${grafanaversion}",
       require  => File["/opt/grafana/${instance_name}/"],
   }
@@ -39,29 +40,19 @@ define profiles::gstack::grafana::instance (
         group   => $grafanauser,
         require => File["/opt/grafana/storage/${instance_name}/conf/"]
   }
-  supervisord::program {
-    "grafana-${instance_name}" :
-      command        => "/opt/grafana/${instance_name}/bin/grafana-server --homepath=/opt/grafana/${instance_name}/ --config=/opt/grafana/storage/${instance_name}/conf/grafana.ini &",
-      stdout_logfile => '/opt/data/log/supervisor/%(program_name)s.log',
-      stderr_logfile => '/opt/data/log/supervisor/%(program_name)s.log',
-      user           => $grafanauser,
-      autostart      => true,
-      autorestart    => true,
-      stopsignal     => 'QUIT',
-      subscribe      => File["/opt/grafana/storage/${instance_name}/conf/grafana.ini"],
-      require        => [
-        Exec["Extract Grafana ${instance_name}"],
-        File["/opt/grafana/storage/${instance_name}/conf/grafana.ini"],
-        Mysql::Db["grafana-${instance_name}"]
-      ]
+  file { "/etc/init.d/grafana-${instance_name}" :
+    content => template('profiles/gstack/service/grafana.init.erb'),
+    group   => $grafanauser,
+    mode    => '0755',
+    notify  => Service["grafana-${instance_name}"],
+    owner   => $grafanauser,
   }
-  exec {
-    "restart-supervisord-${instance_name}" :
-      path        => '/usr/bin:/usr/sbin:/bin',
-      provider    => shell,
-      command     => "supervisorctl restart grafana-${instance_name}",
-      subscribe   => [File["/opt/grafana/storage/${instance_name}/conf/grafana.ini"]],
-      require     => Supervisord::Program["grafana-${instance_name}"],
-      refreshonly => true,
+  service {
+    "grafana-${instance_name}" :
+      ensure   => running,
+      enable   => true,
+      name     => "grafana-${instance_name}",
+      provider => 'redhat',
+      require  => File["/etc/init.d/grafana-${instance_name}"],
   }
 }
